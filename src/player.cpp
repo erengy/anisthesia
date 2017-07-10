1e267371f27b9b6ed599f7957a0b3e198cefe13c
Eren Okka <erenokka@gmail.com>
2017-07-10 21:35:18 +0300

    Add media player data and parser

---
+++ src/player.cpp
@@ -0,0 +1,195 @@
+/*
+MIT License
+
+Copyright (c) 2017 Eren Okka
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "player.h"
+#include "util.h"
+
+namespace anisthesia {
+namespace parser {
+
+enum class State {
+  ExpectPlayerName,
+  ExpectSection,
+  ExpectWindow,
+  ExpectExecutable,
+  ExpectStrategy,
+  ExpectWindowTitle,
+};
+
+size_t GetIndentation(const std::string& line) {
+  return line.find_first_not_of('\t');
+}
+
+bool HandleIndentation(const std::string& line,
+                       const size_t current,
+                       const std::vector<Player>& players,
+                       State& state) {
+  // Each state has a definitive expected indentation
+  const auto expected = [&state]() -> size_t {
+    switch (state) {
+      default:
+      case State::ExpectPlayerName:
+        return 0;
+      case State::ExpectSection:
+        return 1;
+      case State::ExpectWindow:
+      case State::ExpectExecutable:
+      case State::ExpectStrategy:
+        return 2;
+      case State::ExpectWindowTitle:
+        return 3;
+    }
+  }();
+
+  if (current > expected)
+    return false;  // Disallow excessive indentation
+
+  if (current < expected) {
+    auto fix_state = [&]() {
+      state = !current ? State::ExpectPlayerName : State::ExpectSection;
+    };
+    switch (state) {
+      case State::ExpectWindow:
+        if (players.back().windows.empty())
+          return false;
+        fix_state();
+        break;
+      case State::ExpectExecutable:
+        if (players.back().executables.empty())
+          return false;
+        fix_state();
+        break;
+      case State::ExpectStrategy:
+        if (players.back().strategies.empty())
+          return false;
+        fix_state();
+        break;
+      case State::ExpectWindowTitle:
+        return false;
+    }
+  }
+
+  return true;
+}
+
+bool HandleState(std::string& line, std::vector<Player>& players, State& state) {
+  switch (state) {
+    case State::ExpectPlayerName:
+      players.push_back(Player{line});
+      state = State::ExpectSection;
+      break;
+
+    case State::ExpectSection: {
+      static const std::map<std::string, State> sections = {
+        {"windows", State::ExpectWindow},
+        {"executables", State::ExpectExecutable},
+        {"strategies", State::ExpectStrategy},
+      };
+      util::TrimRight(line, ":");
+      const auto it = sections.find(line);
+      if (it == sections.end())
+        return false;
+      state = it->second;
+      break;
+    }
+
+    case State::ExpectWindow:
+      players.back().windows.push_back(line);
+      break;
+
+    case State::ExpectExecutable:
+      players.back().executables.push_back(line);
+      break;
+
+    case State::ExpectStrategy: {
+      static const std::map<std::string, Strategy> strategies = {
+        {"window_title", Strategy::WindowTitle},
+        {"open_files", Strategy::OpenFiles},
+        {"ui_automation", Strategy::UiAutomation},
+      };
+      util::TrimRight(line, ":");
+      const auto it = strategies.find(line);
+      if (it == strategies.end())
+        return false;
+      const auto strategy = it->second;
+      players.back().strategies.push_back(strategy);
+      switch (strategy) {
+        case Strategy::WindowTitle:
+          state = State::ExpectWindowTitle;
+          break;
+      }
+      break;
+    }
+
+    case State::ExpectWindowTitle:
+      players.back().window_title_format = line;
+      state = State::ExpectStrategy;
+      break;
+  }
+
+  return true;
+}
+
+}  // namespace parser
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool ParsePlayersData(const std::string& path, std::vector<Player>& players) {
+  std::string data;
+  if (!util::ReadFile(path, data))
+    return false;
+
+  std::istringstream stream(data);
+  std::string line;
+  size_t indentation = 0;
+  parser::State state = parser::State::ExpectPlayerName;
+
+  while (std::getline(stream, line, '\n')) {
+    if (line.empty())
+      continue;  // Ignore empty lines
+
+    indentation = parser::GetIndentation(line);
+
+    util::TrimLeft(line, "\t");
+    util::TrimRight(line, "\n\r");
+
+    if (line.empty() || line.front() == '#')
+      continue;  // Ignore empty lines and comments
+
+    if (!parser::HandleIndentation(line, indentation, players, state))
+      return false;
+
+    if (!parser::HandleState(line, players, state))
+      return false;
+  }
+
+  return !players.empty();
+}
+
+}  // namespace anisthesia
---
+++ src/player.h
@@ -0,0 +1,62 @@
+/*
+MIT License
+
+Copyright (c) 2017 Eren Okka
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace anisthesia {
+
+enum class Strategy {
+  WindowTitle,
+  OpenFiles,
+  UiAutomation,
+};
+
+enum class PlayerType {
+  Default,
+  WebBrowser,
+};
+
+class Player {
+public:
+  Player() {}
+  Player(const std::string& name) : name(name) {}
+
+  std::string name;
+
+  std::vector<std::string> windows;
+  std::vector<std::string> executables;
+  std::vector<Strategy> strategies;
+
+  std::string window_title_format;
+
+private:
+  PlayerType type_ = PlayerType::Default;
+};
+
+bool ParsePlayersData(const std::string& path, std::vector<Player>& players);
+
+}  // namespace anisthesia
